4172016d8bc4adcff61aad4ca842086b0d2adfb6
tugudush <jerome2kph@gmail.com>
2024-08-09 08:28:18 +0800

    fixed postcss config

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
+export default {
+  plugins: [
+    autoprefixer, // Example plugin
+    cssnano // Example plugin for minification
+  ]
+};
